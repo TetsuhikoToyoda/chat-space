b1ed78912b8aea8116728e30c8584358a3fd118b
TetsuhikoToyoda <sakuyakonohana0717@gmail.com>
2018-12-11 09:31:03 +0900

    Edit javascripts/search.js
    doneメソッドで非同期通信の結果を受け取り、作成したHTMLをviewに埋め込む。

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,17 @@
 $(function() {
+
+var search_list = $("#user-search-result");
+
+function appendUser(user) {
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">
+                  ${user.user_name}
+                </p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.user_name}">追加</a>
+              </div>`
+  search_list.append(html);
+}
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
 
@@ -7,6 +20,18 @@ $(function() {
       url: '/groups/search',
       data: { keyword: input },
       dataType: 'json'
+    })
+
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+    })
+    .fail(function() {
+      alert('ユーザー検索に失敗しました');
     })
   });
 });
